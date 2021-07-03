cd91d58379bcf347b5ef0262f99cc55e3634077a
BozinskyNina <bozinskynina@gmail.com>
2021-07-03 17:23:48 -0300

    Añadida clase ModelSubject

---
+++ Datos/ModelSubject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    class ModelSubject
+    {
+        private int _subjectId;
+        private string _subjectName;
+
+
+        public int subjectId
+        {
+            get { return _subjectId; }
+            set { _subjectId = value; }
+
+        }
+
+        public string subjectName
+        {
+            get { return _subjectName; }
+            set { _subjectName = value; }
+        }
+    }
+
+}
+
